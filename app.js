f84134010273233fa7bb01f2b885d1d3fad0bdbc
Binita rijal <rijalbinita01@gmail.com>
2025-02-12 13:36:49 +0545

    creating schema and model

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const app=require("express")();
-const moongoose=require('mongoose');
 const { connectdatabase } = require("./database/database");
 connectdatabase()
 //connecting to database
+const express=require("express");
+const app=express()
+const moongoose=require('mongoose');
+const Blog=require("./model/blogmodel")
+
 
 
 //get
@@ -12,11 +15,21 @@ app.get("/",(req,res)=>{
         status:200,
         message:"hiii cutieee",
         ifff:true,
-        khattam:"nai bhooo"
+        khattam:"nai bhooo",
+        launa:"uffss"
+    })
+})
+
+app.post("/createBlog",async(req,res)=>{
+  await  Blog.create({
+        title:req.body.title,
+        subTitle:req.body.subTitle,
+        description:req.body.description
     })
 })
 
 app.listen(4000,()=>{
 console.log("port at 4000");
 console.log("Hello soltini");
+console.log("oo k chaa");
 })
